7a7ba0c65c49eb21301c064d8217187454e25c5c
Kevin Stierhoff <kevin.stierhoff@noaa.gov>
2025-06-24 15:21:22 -0700

    Updated plotTrawls to use predefined header values for TV80 files. Fixes a bug where the code fails if the CSV file does not contain a header row. Updated settings accordingly.

--- Doc/settings/settings_2407RL.R
+++ Doc/settings/settings_2407RL.R
@@ -669,7 +669,12 @@ tdr.nav.source         <- "ERDDAP"
 tdr.trawl.source       <- "Access"
 
 # TV80 data
-tv80.offset <- -1 # Offset, in hours
+tv80.offset <- 0 # Offset, in hours
+# TV80 column names
+tv80.cols   <- stringr::str_split("UnixTimeSeconds;DateTime;VES_Latitude;VES_Longitude;VES_Heading;VES_Course_True;VES_Speed;VES_Course_True;VES_Speed;TRAWLEYE_Roll_code;TRAWLEYE_Depth_code;TWL_Depth;TWL_Battery;TWL_Flow1;TWL_Flow1_uncompensated_c;TWL_Geometry_Std;TWL_Pitch;TWL_Geometry_Prt;TWL_Geometry_Dif_c;DOR_Depth_Std;DOR_Temperature_Std;DOR_Battery_Prt;DOR_Roll_Std;DOR_Pitch_Std;DOR_Roll_Prt;DOR_Pitch_Prt;DOR_Depth_Prt;DOR_Depth_Dif_c;DOR_Battery_Prt;DOR_Battery_Std;DOR_Spread;DOR_Battery_Std",
+                                  ";")[[1]]
+# Number of rows to skip when reading TV80 data files
+tv80.skip   <- 0 
 
 # Biomass estimation settings ------------------------------------------
 # Length bins and labels for calculating length frequencies 

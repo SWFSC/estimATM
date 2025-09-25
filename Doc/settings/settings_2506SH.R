eb59b7bf50238a9cc46890015a5e243041dbc66a
Kevin Stierhoff <kevin.stierhoff@noaa.gov>
2025-09-25 16:53:38 -0700

    Added LM backscatter data to analysis.

--- Doc/settings/settings_2506SH.R
+++ Doc/settings/settings_2506SH.R
@@ -334,7 +334,7 @@ lf.ncols <- 5
 # Data sources ------------------------------------------------------------
 # Backscatter data info
 # Survey vessels that collected acoustic data (a character vector of vessel abbreviations)
-nasc.vessels           <- c("SH","LBC") #c("RL","LBC","LM","SD") 
+nasc.vessels           <- c("SH","LBC","LM") #c("RL","LBC","LM","SD") 
 nasc.vessels.offshore  <- NA # c("SD")
 nasc.vessels.nearshore <- c("LBC", "LM")
 nasc.vessels.krill     <- c("SH")

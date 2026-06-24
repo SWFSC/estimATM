bef269ba2e8eb665d66be0574cdee97162b5000f
Josiah Renfree <josiah.renfree@noaa.gov>
2026-06-24 21:05:24 +0000

    Add processMVP.R, adjusted for survey extract_CPS_NASC.R

--- Code/extract_CPS_NASC.R
+++ Code/extract_CPS_NASC.R
@@ -27,17 +27,17 @@ graphics.off()
 # Extract only CPS backscatter -------------------------------------------------
 atm::extract_cps_nasc(
   # Most used options ----------------------------------------------------------
-  path.in     = "C:\\SURVEY\\2506SH\\PROCESSED\\EV\\CSV", # CSV file source                                                                                                 e
+  path.in     = "C:\\SURVEY\\2606RL\\PROCESSED\\EV\\CSV", # CSV file source                                                                                                 e
   pattern.in  = "-Final 38 kHz CPS.csv", # CSV file regex
-  path.out    = "C:\\SURVEY\\2506SH\\PROCESSED\\EV\\CSV", # Processed file destination
+  path.out    = "C:\\SURVEY\\2606RL\\PROCESSED\\EV\\CSV", # Processed file destination
   suffix.out  = "_nasc_cps.csv",             # Suffix applied to processed CSV files
-  path.img    = "C:\\SURVEY\\2506SH\\PROCESSED\\EV\\Exported_Images", # Location of exported image files, or NULL
-  pattern.img = "-38 Remove Passive Pings.png", # Exported image regex
+  path.img    = "C:\\SURVEY\\2606RL\\PROCESSED\\EV\\Exported_Images", # Location of exported image files, or NULL
+  pattern.img = "-38 kHz Sv.png", # Exported image regex
   # Lesser used options --------------------------------------------------------
   expansion   = 2,     # Constant for expanding axes
   max.range   = 350,   # Depth range for bubble plots
   dist.bin    = 2000,  # Distance bins for results plot (2000 default, smaller for short transects)
-  root        = 2,     # Constant for controlling bubble size (2)
+  root        = 2,     # Constant for controlling bubble size (2)2
   scaling     = 0.1,   # Constant for controlling bubble size (0.1)
   jpeg        = TRUE,  # Save intermediate plots from line picks
   x11.w       = 1600,  # Width of graphics window (px)

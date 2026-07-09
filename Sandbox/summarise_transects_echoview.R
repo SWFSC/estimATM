09c27997926cb4cdab385b72f9dcc82c547bfa4c
Kevin Stierhoff <kevin.stierhoff@noaa.gov>
2026-07-09 10:28:41 -0700

    Minor update to plotTrawls and script that summarises the summarisation of start/stop transect times for EV processing.

--- Sandbox/summarise_transects_echoview.R
+++ Sandbox/summarise_transects_echoview.R
@@ -1,7 +1,13 @@
+# Load libraries
+library(tidyverse)
+
+# Load data
+load("C:/KLS/CODE/Github/estimATM/2606RL/Data/Backscatter/nasc_all.Rdata")
 
 # Summarize NASC by transect
 nasc %>% 
   group_by(transect.orig, transect) %>% 
   summarise(transectStart = min(datetime),
             transectEnd = max(datetime)) %>% 
-  arrange(transectStart)
+  arrange(transectStart) %>% 
+  write_csv(here::here("Output/ek80_raw_summary.csv"))

e26e4faaae1e7937aaf89b154c65485b266b05c0
Kevin Stierhoff <kevin.stierhoff@noaa.gov>
2026-07-13 11:15:37 -0700

    Added instructions to install pandoc to batch file.

--- Code/bat/automate-rendering-2606RL.bat
+++ Code/bat/automate-rendering-2606RL.bat
@@ -2,6 +2,8 @@ REM Define the location of the R and Quarto executables
 SET R_PATH="C:\Program Files\R\R-4.6.0\bin\x64\Rscript.exe" 
 SET Q_PATH="C:\Program Files\RStudio\resources\app\bin\quarto\bin\quarto.exe"
 
+REM To install pandoc on Windows, run CMD.exe then winget install JohnMacFarlane.Pandoc
+
 REM Path to where the RMD and QMD files are located
 SET WORK_DIR="C:\KLS\CODE\Github\estimATM\2606RL\Doc"
 

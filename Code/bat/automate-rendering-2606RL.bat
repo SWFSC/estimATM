1c21a54ee65ccbb24350a8adfe1cb29b68e20fd5
Kevin Stierhoff <kevin.stierhoff@noaa.gov>
2026-07-21 10:07:36 -0700

    Updated .bat file to automatically open HTML files from Rmarkdown outputs.

--- Code/bat/automate-rendering-2606RL.bat
+++ Code/bat/automate-rendering-2606RL.bat
@@ -12,14 +12,26 @@ CD /d %WORK_DIR%
 REM Name of plotSurvey Rmd
 SET RMD_FILE="plotSurvey.Rmd"
 %R_PATH% -e "rmarkdown::render('%RMD_FILE%', output_format='html_document')"
+REM Open resulting file
+start "" "%~dp0..\..\Doc\plotSurvey.html"
 
 REM Name of checkTrawls Rmd
 SET RMD_FILE="checkTrawls.Rmd"
 %R_PATH% -e "rmarkdown::render('%RMD_FILE%', output_format='html_document')"
+REM Open resulting file
+start "" "%~dp0..\..\Doc\checkTrawls.html"
+
+REM Name of plotTrawls Rmd
+SET RMD_FILE="plotTrawls.Rmd"
+%R_PATH% -e "rmarkdown::render('%RMD_FILE%', output_format='html_document')"
+REM Open resulting file
+start "" "%~dp0..\..\Doc\plotTrawls.html"
 
 REM Name of trackSurvey Qmd
 SET QMD_FILE="trackSurvey.qmd"
 %Q_PATH% render "%QMD_FILE%"
+REM Open resulting file
+start "" "%~dp0..\..\Doc\trackSurvey.html"
 
 REM Keeps window open to show results
-pause
+REM pause

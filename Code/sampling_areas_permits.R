bf9e3a2c59d3f4d7bacfbd107deb643bba69ad4d
Kevin Stierhoff <kevin.stierhoff@noaa.gov>
2026-08-19 11:22:35 -0700

    Updated script for producing survey area polygons for nearshore and saildrone sampling.

--- Code/sampling_areas_permits.R
+++ Code/sampling_areas_permits.R
@@ -3,38 +3,57 @@ library(sf)
 library(here)
 library(mapview)
 library(atm)
+library(rnaturalearth)
+library(smoothr)
 
-# Define CRSs
+# Define CRS
 crs.geog <- 4326
 crs.proj <- 3310
 
 wpt.regions   <- c("Central CA", "S. CA Bight", "WA/OR", "Santa Cruz Island", "Santa Catalina Island") # "Vancouver Is."        
 
 # Define transect buffering preferences for stratum polygon creation
-na.buffer.dist <- 5 # Distance (nmi) to buffer N. American land mask for core strata masking
+na.buffer.dist <- 1 # Distance (nmi) to buffer N. American land mask for core strata masking
+na.buffer.dist.ns <- 5 # Distance (nmi) to buffer N. American land mask for core strata masking
 ci.buffer.dist <- 2.5 # Distance (nmi) to buffer Channel Island land mask for nearshore strata masking
 ci.clip.dist   <- 0.1 # Distance (nmi) to buffer Channel Island land mask for nearshore strata clipping
 
-# Read North America land mask shapefile and buffer by 5 nmi
-na_buffer <- st_read(here("Data/GIS/na_landmask_final.shp")) %>% 
+# Get countries, buffer by a small amount, and remove interior holes
+na_landmask <- ne_countries(scale = "large", returnclass = "sf") %>%
+  filter(name_en %in% c("United States of America", "Canada")) %>% 
   st_union() %>% 
   st_transform(crs.proj) %>% 
-  st_buffer(dist = na.buffer.dist*1852)
+  st_buffer(dist = na.buffer.dist*1852) %>% 
+  smoothr::fill_holes(threshold = units::set_units(Inf, "m^2"))
 
-# Read Channel Island shapefile and buffer by 2.5 nmi
-ci_buffer <- st_read(here("Data/GIS/channel_islands.shp")) %>%
-  filter(name %in% wpt.regions) %>%
+# Buffer the na_landmask by the typical nearshore transect distance
+na_buffer <- ne_countries(scale = "large", returnclass = "sf") %>%
+  filter(name_en %in% c("United States of America", "Canada")) %>% 
   st_union() %>% 
-  st_transform(crs = crs.proj) %>%
-  st_buffer(dist = ci.buffer.dist * 1852)
+  st_transform(crs.proj) %>% 
+  st_buffer(dist = na.buffer.dist.ns*1852)
+
+bathy_clip <- st_read(here("Data/GIS/bathy_5m_wc.shp")) %>% 
+  st_cast("POLYGON") %>% 
+  st_transform(crs.proj)
+
+# # Read Channel Island shapefile and buffer by 2.5 nmi
+# ci_buffer <- st_read(here("Data/GIS/channel_islands.shp")) %>%
+#   filter(name %in% wpt.regions) %>%
+#   st_union() %>% 
+#   st_transform(crs = crs.proj) %>%
+#   st_buffer(dist = ci.buffer.dist * 1852)
 
 # Read US EEZ shapefile
 eez_usa <- st_read(here("Data/GIS/eez_us.shp")) %>% 
   st_transform(crs.proj)
 
 # Get nearshore survey footprint
-ns_footprint <- st_union(ci_buffer, na_buffer) %>% 
+ns_footprint <- na_buffer %>% 
+  # st_union(ci_buffer, na_buffer) %>% 
   st_intersection(eez_usa) %>% 
+  st_difference(na_landmask) %>% 
+  st_difference(bathy_clip) %>% 
   st_simplify()
 
 # Read offshore clip polygon
@@ -43,6 +62,8 @@ offshore.clip <- st_read(here("Data/GIS/waypoint_polygon.shp")) %>%
 
 # Get the intersection with the US EEZ
 sd_footprint <- st_intersection(offshore.clip, eez_usa) %>% 
+  st_difference(na_landmask) %>% 
+  st_difference(bathy_clip) %>% 
   st_simplify()
 
 # Write to shapefile
@@ -52,4 +73,4 @@ st_write(ns_footprint, here("Data/GIS/Permits/sampling_area_nearshore.shp"),
 st_write(sd_footprint, here("Data/GIS/Permits/sampling_area_saildrone.shp"), 
          delete_layer = TRUE)
 
-mapview(sd_footprint) + mapview(ns_footprint)
+# mapview(sd_footprint) + mapview(ns_footprint)

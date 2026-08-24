5d0bd1195bf1684df98944d19426c73080ea8a85
Kevin Stierhoff <kevin.stierhoff@noaa.gov>
2026-08-24 09:58:31 -0700

    Updated hake color.

--- Doc/settings/settings_2606RL.R
+++ Doc/settings/settings_2606RL.R
@@ -256,7 +256,7 @@ pac.herring.color  <- '#F5DEB3'
 rnd.herring.color  <- '#F0B81D'
 jap.sardine.color  <- '#F0A6A6'
 smelt.color        <- '#A020F0'
-hake.color         <- 'brown'
+hake.color         <- '#563D2D'
 other.color        <- 'gray'
 
 # Set gear type colors
